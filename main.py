af1132962a1cd4360d46aeb93c3e916d765b1749
90duse <duseibnuduse1212@gmail.com>
2024-05-29 13:21:16 +0300

    added comments to simplify the understanding bit more

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#Simple calculator logic app with out UI
+#Simple calculator logic app with out UI using python 
 #we are also using try and except for good coding practice
 
 try:
+    #fist collect the numbers and operator
     num1 = int(input('Enter your First Number: '))
     num2 = int(input('Enter your Second Number: '))
     op = input('Enter Operator: ')
+
+    #then use if statement to know what the user want to do the numbers he provides
     if op == '+':
         print('Your Resul is: ',num1+num2)
     elif op == '-':
